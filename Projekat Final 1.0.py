0044524ac81572e3485ff674ea8061354c55804e
Gapioa <57418108+Gapioa@users.noreply.github.com>
2023-06-06 23:06:12 +0200

    Project Final 1.0.py

--- Projekat Final 1.0.py
+++ Projekat Final 1.0.py
@@ -2,22 +2,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Učitavanje datoteke
-df = pd.read_csv(r'C:\Users\kalma\Desktop\Fax\Treća godina\Treći trimestar\Informacioni sistemi u medicini\ISUM\Heart rate_Feb92023-May82023.csv')
+# Load the data
+df = pd.read_csv(r'C:\Your_path_to_file.csv')
 
-# Konvertovanje kolone 'Time' u String tip podataka
+# Converting column 'Time' to String 
 df['Time'] = df['Time'].astype(str)
 
-# Izdvajanje prvih 50 vrednosti iz baze
+# Selecting first 50 rows of data
 df_first_50 = df.head(50)
 
-# Kreiranje Figure objekta
+# Creating the figure
 fig = plt.figure(figsize=(10, 10))
 
-# Grafik 1 - Heart Rate Signal
+# Graph 1 - Heart Rate Signal
 ax1 = fig.add_subplot(3, 2, (1, 2))
 
-# Oboji vreme sa znakom "-" u zeleno
+#  Color the time with the sign "-" green
 for i, row in df_first_50.iterrows():
     if '-' in row['Time']:
         ax1.plot(row['Time'], row['Heart rate (bpm)'], linestyle='None', marker='o', color='green')
@@ -29,37 +29,37 @@ ax1.set_ylim(50, 170)
 #ax1.set_xlabel('Time')
 ax1.set_ylabel('Heart rate (bpm)')
 ax1.set_title('Heart Rate Signal')
-ax1.tick_params(rotation=60, labelsize=8)  # Promena fonta i rotacije
+ax1.tick_params(rotation=60, labelsize=8)  # Font change and rotation
 ax1.grid(True)
 
-#Grafik 2 - Histogram raspodele srčanih vrednosti (cela baza)
+#Graph 2 - Heart rate distribution histogram (Full Dataset)
 ax2 = fig.add_subplot(3, 2, 3)
 ax2.hist(df['Heart rate (bpm)'], bins=10, edgecolor='black')
-ax2.set_xlabel('Srčani ritam (bpm)')
-ax2.set_ylabel('Broj očitavanja')
-ax2.set_title('Raspodela srčanih vrednosti (Full Dataset)')
+ax2.set_xlabel('Heart rate (bpm)')
+ax2.set_ylabel('Number of readings')
+ax2.set_title('Distribution of heart values (Full Dataset)')
 
-# Grafik 3 - Kumulativni histogram (cela baza)
+# Graph 3 - Cumulative histogram (entire base)
 ax3 = fig.add_subplot(3, 2, 4)
 ax3.hist(df['Heart rate (bpm)'], bins=10, cumulative=True, histtype='bar', edgecolor='black')
-ax3.set_xlabel('Srčani ritam (bpm)')
-ax3.set_ylabel('Kumulativna frekvencija')
-ax3.set_title('Kumulativni histogram (Full Dataset)')
+ax3.set_xlabel('Heart rate (bpm)')
+ax3.set_ylabel('Cumulative frequency')
+ax3.set_title('Cumulative Histogram (Full Dataset)')
 
-# Izračunavanje osnovnih statističkih parametara (cela baza)
-prosek = np.mean(df['Heart rate (bpm)'])
+# Calculation of basic statistical parameters (entire database)
+mean = np.mean(df['Heart rate (bpm)'])
 std = np.std(df['Heart rate (bpm)'])
 kurtosis = df['Heart rate (bpm)'].kurtosis()
 skewness = df['Heart rate (bpm)'].skew()
-medijana = np.median(df['Heart rate (bpm)'])
+median = np.median(df['Heart rate (bpm)'])
 
-# Grafik 4 - Prikaz osnovnih statističkih parametara
+# Graph 4 - Display of basic statistical parameters
 ax4 = fig.add_subplot(3, 2, (5, 6))
 ax4.axis('off')
-ax4.text(0.1, 0.5, f"Prosek: {prosek}\nStandardna devijacija: {std}\nKurtosis: {kurtosis}\nSkewness: {skewness}\nMedijana: {medijana}",
+ax4.text(0.1, 0.5, f"Mean: {mean}\nStandardna devijacija: {std}\nKurtosis: {kurtosis}\nSkewness: {skewness}\nMedijana: {median}",
          fontsize=12)
 
-# Prikaz grafova
+# Display graphs
 plt.tight_layout()
-plt.subplots_adjust(hspace=1)  # Promena razmaka između podgrafika
+plt.subplots_adjust(hspace=1)  # Change the spacing between subplots
 plt.show()
